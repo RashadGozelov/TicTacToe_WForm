25d182d78d45beb3e97f3f683c77c4dd0b19e3f2
RashadGozelov <rashadVg@code.edu.az>
2020-05-17 18:47:18 +0400

    TicTacToe_Game

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlButtons = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LblTurn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LblPlayer2Score = new System.Windows.Forms.Label();
+            this.LblPlayer1Score = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.BtnPlayAgain = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlButtons
+            // 
+            this.PnlButtons.BackgroundImage = global::TicTacToe.Properties.Resources._512x512bb;
+            this.PnlButtons.Location = new System.Drawing.Point(0, 0);
+            this.PnlButtons.Name = "PnlButtons";
+            this.PnlButtons.Size = new System.Drawing.Size(270, 509);
+            this.PnlButtons.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.LblTurn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(313, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 68);
+            this.panel1.TabIndex = 1;
+            // 
+            // LblTurn
+            // 
+            this.LblTurn.AutoSize = true;
+            this.LblTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTurn.Location = new System.Drawing.Point(72, 37);
+            this.LblTurn.Name = "LblTurn";
+            this.LblTurn.Size = new System.Drawing.Size(0, 22);
+            this.LblTurn.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(51, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sıradakı";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.LblPlayer2Score);
+            this.panel2.Controls.Add(this.LblPlayer1Score);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(313, 199);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(174, 68);
+            this.panel2.TabIndex = 2;
+            // 
+            // LblPlayer2Score
+            // 
+            this.LblPlayer2Score.AutoSize = true;
+            this.LblPlayer2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPlayer2Score.Location = new System.Drawing.Point(123, 37);
+            this.LblPlayer2Score.Name = "LblPlayer2Score";
+            this.LblPlayer2Score.Size = new System.Drawing.Size(17, 18);
+            this.LblPlayer2Score.TabIndex = 4;
+            this.LblPlayer2Score.Text = "0";
+            // 
+            // LblPlayer1Score
+            // 
+            this.LblPlayer1Score.AutoSize = true;
+            this.LblPlayer1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPlayer1Score.Location = new System.Drawing.Point(31, 37);
+            this.LblPlayer1Score.Name = "LblPlayer1Score";
+            this.LblPlayer1Score.Size = new System.Drawing.Size(17, 18);
+            this.LblPlayer1Score.TabIndex = 3;
+            this.LblPlayer1Score.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Green;
+            this.label4.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(90, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Oyunçu 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Green;
+            this.label3.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Oyunçu 1";
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.Location = new System.Drawing.Point(313, 350);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(174, 33);
+            this.BtnReset.TabIndex = 3;
+            this.BtnReset.Text = "Sıfırla";
+            this.BtnReset.UseVisualStyleBackColor = false;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // BtnPlayAgain
+            // 
+            this.BtnPlayAgain.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnPlayAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlayAgain.Location = new System.Drawing.Point(313, 389);
+            this.BtnPlayAgain.Name = "BtnPlayAgain";
+            this.BtnPlayAgain.Size = new System.Drawing.Size(174, 33);
+            this.BtnPlayAgain.TabIndex = 4;
+            this.BtnPlayAgain.Text = "Yenidən Oyna";
+            this.BtnPlayAgain.UseVisualStyleBackColor = false;
+            this.BtnPlayAgain.Click += new System.EventHandler(this.BtnPlayAgain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TicTacToe.Properties.Resources._512x512bb;
+            this.ClientSize = new System.Drawing.Size(514, 509);
+            this.Controls.Add(this.BtnPlayAgain);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PnlButtons);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlButtons;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LblTurn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblPlayer2Score;
+        private System.Windows.Forms.Label LblPlayer1Score;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Button BtnPlayAgain;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public enum Turn
+    {
+        X,
+        O
+    }
+
+    public enum Player
+    {
+        Player1,
+        Player2
+    }
+    public partial class Form1 : Form
+    {
+        private List<Button> _buttons;
+        private Turn _currentTurn;
+        private Player _currentPlayer;
+        private int _player1Score;
+        private int _player2Score;
+        private bool _isResume;
+        public Form1()
+        {
+            InitializeComponent();
+            _buttons = new List<Button>();
+            CreateButton();
+            this._currentTurn = Turn.X;
+            this._currentPlayer = Player.Player1;
+
+            LblTurn.Text = Turn.X.ToString();
+            this.DisplayScore();
+            this._isResume = true;
+        }
+
+        private void CreateButton()
+        {
+            int x = 30;
+            int y = 50;
+            for (int i = 0; i < 9; i++)
+            {
+                Button button = new Button();
+                button.Size = new Size(70, 70);
+                button.Font = new Font("Microsoft San Serif", 15,FontStyle.Bold);
+                button.Location = new Point(x, y);
+                button.BackColor = Color.LightGray;
+                button.TabStop = false;
+                button.FlatStyle = FlatStyle.Flat;
+                button.FlatAppearance.BorderSize = 0;
+                button.Click += new EventHandler(AllButtonClick);
+
+                PnlButtons.Controls.Add(button);
+                _buttons.Add(button);
+                x += 80;
+                if ((i + 1) % 3 == 0) 
+                {
+                    y += 150;
+                    x = 30;
+                }
+              
+            }
+            
+        }
+
+        private void AllButtonClick(object sender,EventArgs e)
+        {
+            Button clicked = sender as Button;
+            if (clicked.Text==string.Empty && this._isResume)
+            {
+                clicked.Text = this._currentTurn.ToString();
+
+                if (CheckWinner())
+                {
+                    this._isResume = false;
+                    return;
+                }
+                this.ChangeTurn();
+            }
+        }
+
+        private void ChangeTurn()
+        {
+            if (this._currentTurn == Turn.X)
+            {
+                this._currentTurn = Turn.O;
+            }
+            else
+            {
+                this._currentTurn = Turn.X;
+            }
+
+            if (this._currentPlayer==Player.Player1)
+            {
+                this._currentPlayer = Player.Player2;
+            }
+            else
+            {
+                this._currentPlayer = Player.Player1;
+            }
+            this.LblTurn.Text = this._currentTurn.ToString();
+        }
+
+        private bool CheckWinner()
+        {
+            if (this._buttons[0].Text != string.Empty &&
+                this._buttons[0].Text == this._buttons[1].Text &&
+                this._buttons[1].Text == this._buttons[2].Text)
+            {
+                // 1 2 3 Winner
+                this.DisplayWinner(new int[] { 0, 1, 2 });
+                return true;
+                 //MessageBox.Show()
+            }else if(this._buttons[3].Text != string.Empty &&
+                     this._buttons[3].Text == this._buttons[4].Text &&
+                     this._buttons[4].Text == this._buttons[5].Text)
+            {
+                // 4 5 6 Winner
+                this.DisplayWinner(new int[] { 3, 4, 5 });
+                return true;
+            }
+            else if (this._buttons[6].Text != string.Empty &&
+                     this._buttons[6].Text == this._buttons[7].Text &&
+                     this._buttons[7].Text == this._buttons[8].Text)
+            {
+                // 7 8 9 Winner
+                this.DisplayWinner(new int[] { 6, 7, 8 });
+                return true;
+            }
+            else if (this._buttons[0].Text != string.Empty &&
+                     this._buttons[0].Text == this._buttons[3].Text &&
+                     this._buttons[3].Text == this._buttons[6].Text)
+            {
+                // 0 4 7 Winner
+                this.DisplayWinner(new int[] { 0, 3, 6 });
+                return true;
+            }
+            else if (this._buttons[1].Text != string.Empty &&
+                     this._buttons[1].Text == this._buttons[4].Text &&
+                     this._buttons[4].Text == this._buttons[7].Text)
+            {
+                // 2 5 8 Winner
+                this.DisplayWinner(new int[] { 1, 4, 7 });
+                return true;
+            }
+            else if (this._buttons[2].Text != string.Empty &&
+                     this._buttons[2].Text == this._buttons[5].Text &&
+                     this._buttons[5].Text == this._buttons[8].Text)
+            {
+                // 3 6 9 Winner
+                this.DisplayWinner(new int[] { 2, 5, 8 });
+                return true;
+            }
+            else if (this._buttons[0].Text != string.Empty &&
+                     this._buttons[0].Text == this._buttons[4].Text &&
+                     this._buttons[4].Text == this._buttons[8].Text)
+            {
+                // 1 5 9 Winner
+                this.DisplayWinner(new int[] { 0, 4, 8 });
+                return true;
+            }
+            else if (this._buttons[2].Text != string.Empty &&
+                     this._buttons[2].Text == this._buttons[4].Text &&
+                     this._buttons[4].Text == this._buttons[6].Text)
+            {
+                // 3 5 7 Winner
+                this.DisplayWinner(new int[] { 2, 4, 6 });
+                return true;
+            }
+            if (!this._buttons.Exists(b=>b.Text==string.Empty))
+            {
+                MessageBox.Show("Oyun bərabərə bitdi!","TicTacToe",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return true;
+            }
+            return false;
+        }
+
+        private void DisplayWinner(int[] a)
+        {
+            foreach (int item in a)
+            {
+                this._buttons[item].BackColor = Color.Green;
+            }
+            if (this._currentPlayer==Player.Player1)
+            {
+                this._player1Score++;
+            }
+            else
+            {
+                this._player2Score++;
+            }
+            DisplayScore();
+        }
+
+        private void DisplayScore()
+        {
+            LblPlayer1Score.Text = this._player1Score.ToString();
+            LblPlayer2Score.Text = this._player2Score.ToString();
+        }
+
+        private void BtnPlayAgain_Click(object sender, EventArgs e)
+        {
+            this.ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            foreach (Button item in this._buttons)
+            {
+                item.BackColor = Color.LightGray;
+                item.Text = string.Empty;
+            }
+            this._currentTurn = Turn.X;
+            this._currentPlayer = Player.Player1;
+
+            LblTurn.Text = Turn.X.ToString();
+            this._isResume = true;
+
+        }
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.ResetGame();
+            this._player1Score = 0;
+            this._player2Score = 0;
+            this.DisplayScore();
+        }
+    }
+}
